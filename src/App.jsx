96c732131a6730c6aee0eb091b91f52ecc6824e4
Frandenilson Antony <iabyantony88@gmail.com>
2024-06-14 05:12:55 +0000

    Criação do HTML e CSS para o Card

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-     App
+      <div className='card'>
+        <h1>Java</h1>
+        <img src='https://salvatore.academy/devmon/1_java.png' width="100" />
+      </div>
     </>
   )
 }
